055066aa30d92fc030cbdea8cc290f641847f0d1
Alexa Guerrero López <99861188+Guexa@users.noreply.github.com>
2024-01-31 19:02:32 -0600

    31/01/2024 - AGL

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import Form
 from wtforms import StringField, EmailField, IntegerField
 
 class UserForm(Form):
-    nombre = StringField('nombre')
-    a_paterno = StringField('apaterno')
-    a_materno = StringField('amaterno')
-    email = EmailField('email')
-    edad = IntegerField('edad')
+    nombre = StringField('Nombre')
+    a_paterno = StringField('Primer Apellido')
+    a_materno = StringField('Segundo Apellido')
+    email = EmailField('Email')
+    edad = IntegerField('Edad')
--- main.py
+++ main.py
@@ -17,8 +17,15 @@ def alumnos():
     # )
     alumno_clase = forms.UserForm(request.form)
     if request.form == 'POST' :
-        pass 
-    return render_template("alumnos.html", form = alumno_clase)
+        nom = alumno_clase.nombre.data
+        p_ap = alumno_clase.a_paterno.data
+        s_ap = alumno_clase.a_materno.data
+        edad = alumno_clase.edad.data
+    
+        print('Nombre {} - Primer Apellido {} - Segundo Apellido {} - Edad {} - Edad {}'
+              .format(nom, p_ap, s_ap, edad))
+    return render_template("alumnos.html", 
+                form = alumno_clase, nom = nom, p_ap = p_ap, s_ap = s_ap)
 
 @app.route("/maestros")
 def maestros():
